eb23a581e4b035dc13eb990f1dd935d832aac9e1
Aiden Jones <20084545@tafe.wa.edu.au>
2024-04-24 17:27:05 +0800

    Add: hello command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import sys
 
 class MyClient(discord.Client):
@@ -8,7 +9,14 @@ class MyClient(discord.Client):
 # This example requires the 'message_content' intent.
     async def on_message(self, message):
         print(f'Message from {message.author} in {message.guild}: {message.content}')
+        if message.author.id == self.user.id:
+            return
 
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+            
+            
+               
 intents = discord.Intents.default()
 intents.message_content = True
 
